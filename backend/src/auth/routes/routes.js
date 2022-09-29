28de6b2ee3057bcb64a7dfc0b82d584f11efc319
Jeffrey <97353049+Delacrobix@users.noreply.github.com>
2022-09-28 19:19:00 -0500

    Inicio de la implementacion de registro por redes sociales e implementacion de la interfaz grafica del login

---
+++ backend/src/auth/routes/routes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import controllers from "../controllers/login-controllers";
+import passport from 'passport';
+import GoogleStrategy from 'passport-google-oidc';
+
+const router = express.Router();
+
+/**
+ * *Rutas dedicadas a renderizar las vistas.
+ */
+router.get('/auth/login', function (req, res) {
+  res.render('login', {});
+});
+
+router.get('/login/federated/google', passport.authenticate('google'));
+
+/**
+ * *Rutas dedicadas al login.
+ */
+router.route("/addUser").post(controllers.addUser);
+router.route("/getUser/:userId").get(controllers.findUserById);
+
+export default router;
